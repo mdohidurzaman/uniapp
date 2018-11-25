f1e27f86be35aeddf4b5cffb62266b7b95c5d7c6
Md Ohidurzaman <zamanohidur@yahoo.com>
2018-11-25 09:38:16 +0000

    increase showing content content

--- app/controllers/student_courses_controller.rb
+++ app/controllers/student_courses_controller.rb
@@ -4,7 +4,7 @@ class StudentCoursesController < ApplicationController
       course_to_add = Course.find(params[:course_id])
       unless current_user.courses.include?(course_to_add)
       StudentCourse.create(course: course_to_add, student: current_user)
-      flash[:notice] = 'You have successfully enrolled in #{course_to_add.name}'
+      flash[:notice] = "You have successfully enrolled in #{course_to_add.name}"
       redirect_to current_user
    else
       flash[:notice] = 'Something was wrong with your enrollment'
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,7 @@
 class Course < ApplicationRecord
     validates :short_name, presence: true, length: { minimun: 5, maximum: 15 }
     validates :name, presence: true, length: { minimun: 10, maximum: 50 }
-    validates :description, presence: true, length: { minimun: 20, maximum: 300 }
+    validates :description, presence: true, length: { minimun: 20, maximum: 400 }
     has_many :student_courses
     has_many :students, through: :student_courses
 end
